6b7d279e635c7b66c5855dc01bd5674d580530cf
DeBray Carpenter <debraycarpenter@DeBrays-MacBook-Pro.local>
2017-04-24 13:37:10 -0700

    upated files

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const app = express()
+const fs = require('fs')
+const path = require('path')
+const util = require('util')
+const request = require('request')
+const cheerio = require('cheerio')
+
+
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', () => {
+app.get('/', (req, res) => {
   res.render('index')
 })
 
